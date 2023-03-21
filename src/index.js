90a414dda38e213c33d0c3f6130c9a49a8edcacf
svitbar <svitbar1102@gmail.com>
2023-03-21 18:10:09 +0200

    Implement six methods of class MyList
    Implement length, append, insert, delete, deleteAll and get methods.
    Demonstate the usage of these methods.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,30 @@ const MyList = require('./list');
 
 const list = new MyList;
 
+console.log(list.length());
+
+list.append('L');
+list.append('й');
+list.append('a');
+list.append('안');
+
+console.log(list.length());
+console.log(list);
+
+list.insert('a', 0);
+list.insert('J', 1);
+list.insert('a', 3);
+
+console.log(list.length());
+console.log(list);
+
+console.log(list.delete(1));
+
+list.deleteAll('a');
+list.deleteAll();
+
+console.log(list.length());
+console.log(list);
+
+console.log(list.get(1));
+
--- src/list.js
+++ src/list.js
@@ -3,12 +3,65 @@ class MyList {
     this.items = [];
   }
 
-  length() {}
-  append(element) {}
-  insert(element, index) {}
-  delete(index) {}
-  deleteAll(element) {}
-  get(index) {}
+  length() {
+    return this.items.length;
+  }
+
+  append(el) {
+    if (typeof el !== 'string' && el.length !== 1) {
+      throw new Error('Element must be a string of length 1');
+    } else if (!RegExp(/^\p{L}/, 'u').test(el)) {
+      throw new Error('Element must be a char');
+    } else this.items.push(el);
+  }
+
+  insert(el, index) {
+    const err = 'Index must be in the range from 0 to ';
+
+    if (typeof el !== 'string' && el.length !== 1) {
+      throw new Error('Element must be a string of length 1');
+    } else if (!RegExp(/^\p{L}/, 'u').test(el)) {
+      throw new Error('Element must be a char');
+    } else if (typeof index !== 'number') {
+      throw new Error('Index must be a number');
+    } else if (index < 0 || index >= this.items.length) {
+      throw new Error(err + `${this.items.length - 1}`);
+    } else this.items.splice(index, 0, el);
+  }
+
+  delete(index) {
+    const err = `Index must be in the range from 0 to ${this.items.length - 1}`;
+    let res;
+
+    if (typeof index !== 'number') {
+      throw new Error('Index must be a number');
+    } else if (index < 0 || index >= this.items.length) {
+      throw new Error(err);
+    } else {
+      res = this.items[index];
+      this.items.splice(index, 1);
+    }
+
+    return res;
+  }
+
+  deleteAll(el) {
+    this.items = this.items.filter((item) => item !== el);
+  }
+
+  get(index) {
+    const err = `Index must be in the range from 0 to ${this.items.length - 1}`;
+    let res;
+
+    if (typeof index !== 'number') {
+      throw new Error('Index must be a number');
+    } else if (index < 0 || index >= this.items.length) {
+      throw new Error(err);
+    } else res = this.items[index];
+
+    return res;
+  }
+
   clone() {}
   reverse() {}
   findFirst(element) {}
